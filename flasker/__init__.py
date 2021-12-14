d5aace57d733f85cc11c4667d4dd277e75750869
iddowaxman <iddow@aidoc.com>
2021-12-14 14:58:52 +0200

    Updated gitignore to exclude .idea folder.

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -2,16 +2,17 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flasket.sqlite'),
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flasker.sqlite'),
     )
 
     if test_config is None:
-        # load the onstance config, if it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
@@ -35,4 +36,3 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     return app
-
